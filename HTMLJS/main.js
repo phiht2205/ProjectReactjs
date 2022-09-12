8b1170991ba79a9c1a01bb4f065ea5d2a3b13df8
PhiHT <phiht2205@gmail.com>
2022-09-12 20:33:46 +0700

    Commit React project

---
+++ HTMLJS/main.js
@@ -0,0 +1,14 @@
+var myObject = {
+    name: 'PhiHT',
+    age: '30',
+};
+
+var myArray = [
+    PhiHT = [
+        'PhiHT-1',
+        'PhiHT-2'
+    ],
+    '30'
+];
+
+console.log('myobject', myArray[0][0]);
